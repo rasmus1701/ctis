2db5f3e142fed7ff5d32882b79b302b558a68e40
Rasmus Andersen <rasmus@newtec.dk>
2017-08-28 13:08:29 +0200

    More styling
    Change-Id: I8c9d3462b8c6104756bf9389be33f3ffa0bb42fd

--- ctis_runner.m
+++ ctis_runner.m
@@ -1,6 +1,7 @@
 dim = 3;
 lambda = 5;
 iterations = 10;
+
 if nargin == 3
     dim = str2num(argv(){1});
     lambda = str2num(argv(){2});
@@ -11,8 +12,6 @@ else
 end
 printf("Computing CTIS image for system of dimension %d and %d spectral layers, using %d iterations\n", dim, lambda, iterations);
 
-%run("h_generator")
-%run("ctis_image_generator");
 H = h_generator(dim, lambda);
 [f, g] = ctis_image_generator(dim, lambda);
 f_calc = exp_max(H, g, iterations, dim, lambda);
--- h_generator.m
+++ h_generator.m
@@ -4,63 +4,59 @@
 
 function [H] = h_generator(d, lambda)
     h_iter = 1;
-    H = zeros(9*d*d+12*d*lambda+4*lambda*lambda, d*d*lambda);
+    H = zeros(9*d*d + 12*d*lambda + 4*lambda*lambda, d*d*lambda);
 
-    # looper igennem alle punkter i datakuben (x, y, lambda)
+    % loop through data cube (x, y, lambda)
     for x_iter = 1:d
         for y_iter = 1:d
             for lambda_iter = 1:lambda
                 dist = 0;
                 Z = d*3 + lambda*2;
                 midt = floor((Z)/2);
-                
-                # laver et tomt billede
-                background = zeros(Z, Z);
-                image = background;
+
+                % make empty image
+                image = zeros(Z, Z);
 
                 % make datacube with 0s
-                m = zeros(d, d, lambda);
-                
-                # sætter een voxel i kuben til 1 - denne skiftes for hver iteration.
-                m(x_iter, y_iter, lambda_iter) = 1;
+                f = zeros(d, d, lambda);
+
+                % sætter een voxel i kuben til 1 - denne skiftes for hver iteration.
+                f(x_iter, y_iter, lambda_iter) = 1;
                 mSum = zeros(d, d);
 
-                # beregner 0te orden ved at summe alle spektral lag
+                % beregner 0te orden ved at summe alle spektral lag
                 for i = 1:lambda
-                    mSum = mSum+m(:, :, i);
+                    mSum = mSum + f(:, :, i);
                 end
 
-                # populate 0th order generated in the image
+                % populate 0th order generated in the image
                 for i = 1:d
                     for j = 1:d
                       image(midt-(d-1)/2+i, midt-(d-1)/2+j) = mSum(i, j);
                     end
                 end
 
-                image = image*d/lambda;
+                image = image*d / lambda;
 
                 % populate 1st order diffractions
                 for h = 1:lambda
                     for i = 1:d
-                      for j = 1:d
-                        image(midt-(d-1)/2+i+(d+dist+h), midt-(d-1)/2+j) = image(midt-(d-1)/2+i+d+dist+h, midt-(d-1)/2+j)+m(i, j, h);    #Bottom
-                        image(midt-(d-1)/2+i, midt-(d-1)/2+j+(d+dist+h)) = image(midt-(d-1)/2+i, midt-(d-1)/2+j+d+dist+h)+m(i, j, h);    #Right
-                        image(midt-(d-1)/2+i-(d+dist+h), midt-(d-1)/2+j) = image(midt-(d-1)/2+i-d-dist-h, midt-(d-1)/2+j)+m(i, j, h);    #Top
-                        image(midt-(d-1)/2+i, midt-(d-1)/2+j-(d+dist+h)) = image(midt-(d-1)/2+i, midt-(d-1)/2+j-d-dist-h)+m(i, j, h);    #Left
-                        image(midt-(d-1)/2+i+(d+dist+h), midt-(d-1)/2+j+(d+dist+h)) = image(midt-(d-1)/2+i+(d+dist+h), midt-(d-1)/2+j+(d+dist+h))+m(i, j, h);  #Bottom-Right
-                        image(midt-(d-1)/2+i+(d+dist+h), midt-(d-1)/2+j-(d+dist+h)) = image(midt-(d-1)/2+i+(d+dist+h), midt-(d-1)/2+j-(d+dist+h))+m(i, j, h);  #Bottom-Left
-                        image(midt-(d-1)/2+i-(d+dist+h), midt-(d-1)/2+j+(d+dist+h)) = image(midt-(d-1)/2+i-(d+dist+h), midt-(d-1)/2+j+(d+dist+h))+m(i, j, h);  #Top-Right
-                        image(midt-(d-1)/2+i-(d+dist+h), midt-(d-1)/2+j-(d+dist+h)) = image(midt-(d-1)/2+i-(d+dist+h), midt-(d-1)/2+j-(d+dist+h))+m(i, j, h);  #Top-Left
-                      end
+                        for j = 1:d
+                            image(midt-(d-1)/2+i+(d+dist+h), midt-(d-1)/2+j) = image(midt-(d-1)/2+i+d+dist+h, midt-(d-1)/2+j) + f(i, j, h);    %Bottom
+                            image(midt-(d-1)/2+i, midt-(d-1)/2+j+(d+dist+h)) = image(midt-(d-1)/2+i, midt-(d-1)/2+j+d+dist+h) + f(i, j, h);    %Right
+                            image(midt-(d-1)/2+i-(d+dist+h), midt-(d-1)/2+j) = image(midt-(d-1)/2+i-d-dist-h, midt-(d-1)/2+j) + f(i, j, h);    %Top
+                            image(midt-(d-1)/2+i, midt-(d-1)/2+j-(d+dist+h)) = image(midt-(d-1)/2+i, midt-(d-1)/2+j-d-dist-h) + f(i, j, h);    %Left
+                            image(midt-(d-1)/2+i+(d+dist+h), midt-(d-1)/2+j+(d+dist+h)) = image(midt-(d-1)/2+i+(d+dist+h), midt-(d-1)/2+j+(d+dist+h)) + f(i, j, h);  %Bottom-Right
+                            image(midt-(d-1)/2+i+(d+dist+h), midt-(d-1)/2+j-(d+dist+h)) = image(midt-(d-1)/2+i+(d+dist+h), midt-(d-1)/2+j-(d+dist+h)) + f(i, j, h);  %Bottom-Left
+                            image(midt-(d-1)/2+i-(d+dist+h), midt-(d-1)/2+j+(d+dist+h)) = image(midt-(d-1)/2+i-(d+dist+h), midt-(d-1)/2+j+(d+dist+h)) + f(i, j, h);  %Top-Right
+                            image(midt-(d-1)/2+i-(d+dist+h), midt-(d-1)/2+j-(d+dist+h)) = image(midt-(d-1)/2+i-(d+dist+h), midt-(d-1)/2+j-(d+dist+h)) + f(i, j, h);  %Top-Left
+                      	end
                     end
                 end
 
-                #kalder datakuben for f for at synkronisere med litteratur
-                f = m;
+                % smider g-vektoren ind som en søjle i H kuben
                 S = size(image, 1);
                 g = reshape(image, S*S, 1);
-
-                # smider g-vektoren ind som en søjle i H kuben
                 H(:, h_iter) = g;
                 h_iter = h_iter + 1;
             end
